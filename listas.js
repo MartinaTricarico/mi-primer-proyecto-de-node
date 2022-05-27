787d2d7e3842ba0b7c1e7900b49cfb5c2bce4b66
Martina Tricarico <tricaricomartina4@gmail.com>
2022-05-27 15:11:16 -0300

    lista

---
+++ listas.js
@@ -0,0 +1,7 @@
+const lista = ["Martina", 22, true, 01 / 11 / 1999];
+const libroFavorito = {
+  titulo: "Peter Pan y sus mujeres",
+  autor: "Adriana Guraieb",
+  fecha: 2012,
+  url: "https://www.intersubjetividad.com.ar/peter-pan-y-sus-mujeres-adriana-guraieb/",
+};
